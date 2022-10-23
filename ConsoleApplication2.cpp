81a710792e5842981d3a29d483254d82341d483d
FelixVoro <115408780+FelixVoro@users.noreply.github.com>
2022-10-23 15:27:01 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n,a,b;
+	cin >> n;
+	b = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		b += a;
+	}
+	cout << float(b) / float(n);
+}
